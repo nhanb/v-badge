9cd8b34d594d9b6d8e4306ff37ab222864905385
nhanb <nhan@nerdyweekly.com>
2016-10-26 14:24:47 +0700

    include fighter ID, read & write image

--- test_vbadge.py
+++ test_vbadge.py
@@ -5,11 +5,31 @@ import requests_cache
 requests_cache.install_cache('requests_cache')
 
 
+class TestCreateBadge(unittest.TestCase):
+
+    def test_read_badge(self):
+        vbadge.create_badge({
+            'fighter_id': 'Rauden',
+            'updated_at': '2016-10-25',
+            'rank': '25459',
+            'lp': '4525',
+            'league': 'Gold',
+            'fav_char': 'Ryu',
+            'account': 'STEAM',
+            'country': 'Viet Nam',
+            'ranked_counts': (621, 1228),
+            'casual_counts': (45, 83),
+        })
+
+        self.assertTrue(False)
+
+
 class TestGetProfile(unittest.TestCase):
 
     def test_get_rauden(self):
         profile = vbadge.get_profile('rauden')
         self.assertEqual(profile, {
+            'fighter_id': 'Rauden',
             'updated_at': '2016-10-25',
             'rank': '25459',
             'lp': '4525',
--- vbadge.py
+++ vbadge.py
@@ -1,11 +1,36 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+from PIL import Image
 
 VLEAGUE_URL = 'https://v-league.pro/player/%s/profile'
+CHAR_DIR = 'characters'
+BADGE_DIR = 'badges'
+
+
+if not os.path.isdir(BADGE_DIR):
+    os.mkdir(BADGE_DIR)
+
+
+def create_badge(profile):
+    infile = '%s/%s.jpg' % (CHAR_DIR, _character_filename(profile['fav_char']))
+    image = Image.open(infile)
+
+    # Write text on image
+    # TODO
+
+    outfile = '%s/%s.png' % (BADGE_DIR, profile['fighter_id'].lower())
+    image.save(outfile)
+    image.show()
+
+
+def _character_filename(character_name):
+    return character_name.lower().replace('.', '')
 
 
 def get_profile(fighter_id):
-    resp = requests.get(VLEAGUE_URL % fighter_id.lower())
+    fighter_id = fighter_id.lower()
+    resp = requests.get(VLEAGUE_URL % fighter_id)
     resp.raise_for_status()
     soup = BeautifulSoup(resp.content, 'lxml')
 
@@ -24,7 +49,19 @@ def get_profile(fighter_id):
         ('casual_counts', 'Casual Match', split_counts),
     ]
 
-    return {key: _field_value(soup, *args) for key, *args in fields}
+    result = {key: _field_value(soup, *args) for key, *args in fields}
+
+    result['fighter_id'] = fighter_id
+    # Look for fighter ID in proper case
+    breadcrumbs = soup.find('ol', class_='breadcrumb').children
+    for b in breadcrumbs:
+        if (hasattr(b, 'text')):
+            text = b.text.strip()
+            if text.lower() == fighter_id:
+                result['fighter_id'] = text
+                break
+
+    return result
 
 
 def _field_value(soup, field_name, transform=lambda x: x):
